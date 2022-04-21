5986e46320f03e4a2c2ffd04447b516bf28e0e1a
FilipeMarx13 <54817989+FilipeMarx13@users.noreply.github.com>
2022-04-21 18:58:53 -0300

    Creation of customer and order controller and update launchUrl at launchsettings

---
+++ src/customer-order-ilia.Api/Controllers/CustumerController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace customer_order_ilia.Api.Controllers
+{
+    /// <summary>
+    /// Controller Customer
+    /// </summary>
+    [ApiController]
+    [Route("customer-order-ilia/customer")]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ src/customer-order-ilia.Api/Controllers/OrderController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace customer_order_ilia.Api.Controllers
+{
+    /// <summary>
+    /// Controller Order
+    /// </summary>
+    [ApiController]
+    [Route("customer-order-ilia/order")]
+    public class OrderController : ControllerBase
+    {
+        public OrderController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
